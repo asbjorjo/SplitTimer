70c23b90ae1cdd194703b6db07e064ec5976cb25
Asbjoern L. Johansen <asbjorjo@gmail.com>
2016-11-23 00:05:44 +0100

    Changed pretty much everything.

--- app/src/main/java/org/asbjorjo/splittimer/MainActivity.java
+++ app/src/main/java/org/asbjorjo/splittimer/MainActivity.java
@@ -1,25 +1,30 @@
 package org.asbjorjo.splittimer;
 
+import android.app.FragmentManager;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.TableLayout;
-import android.widget.TableRow;
-import android.widget.TextView;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+import android.widget.Spinner;
 
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.List;
 
+import de.codecrafters.tableview.SortableTableView;
 import de.codecrafters.tableview.TableView;
-import de.codecrafters.tableview.toolkit.SimpleTableDataAdapter;
+import de.codecrafters.tableview.listeners.TableDataClickListener;
 
 public class MainActivity extends AppCompatActivity {
-    ArrayList<Athlete> athletes = new ArrayList<>();
+    DataFragment data;
+    List<Athlete> athletes;
+    Athlete reference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,36 +33,37 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-       FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        FragmentManager fm = getFragmentManager();
+        data = (DataFragment) fm.findFragmentByTag("data");
 
-        createAthletes();
+        if (data == null) {
+            data = new DataFragment();
+            fm.beginTransaction().add(data, "data").commit();
+            createAthletes();
+            data.setAthletes(athletes);
+        }
+        athletes = data.getAthletes();
+        reference = data.getReference();
+
         initializeTable();
+        initializeDropdown();
     }
 
-    private void initializeTable() {
-        TableView table = (TableView) findViewById(R.id.main_table);
-        table.setDataAdapter(new AthleteTableDataAdapter(this, athletes));
-
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        data.setAthletes(athletes);
+        data.setReference(reference);
     }
 
-    private void createAthletes() {
-        Calendar cal = Calendar.getInstance();
-        athletes.add(new Athlete("Arne", 1L, cal.getTimeInMillis()));
-        cal.add(Calendar.MINUTE, 5);
-        athletes.add(new Athlete("Per", 2L, cal.getTimeInMillis()));
-        cal.add(Calendar.MINUTE, 5);
-        athletes.add(new Athlete("Kari", 3L, cal.getTimeInMillis()));
-        cal.add(Calendar.MINUTE, 5);
-        athletes.add(new Athlete("Kjersti", 4L, cal.getTimeInMillis()));
-        cal.add(Calendar.MINUTE, 5);
-        athletes.add(new Athlete("Kjell", 5L, cal.getTimeInMillis()));
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        super.onSaveInstanceState(savedInstanceState);
+    }
+
+    @Override
+    public void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
     }
 
     @Override
@@ -80,5 +86,87 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private void initializeTable() {
+        SortableTableView table = (SortableTableView) findViewById(R.id.main_table);
+        table.setDataAdapter(new AthleteTableDataAdapter(this, athletes));
+        table.setColumnCount(3 + athletes.get(0).intermediates.length);
+        table.setColumnComparator(0, new Athlete.NameComparator());
+        table.setColumnComparator(1, new Athlete.NumberComparator());
+        table.setColumnComparator(2, new Athlete.StartComparator());
+
+        for (int i = 0; i < athletes.get(0).intermediates.length; i++) {
+            int column = 3+i;
+            table.setColumnComparator(column, new AthleteIntermediateComparator(i));
+        }
+
+        table.addDataClickListener(new AthleteClickListener());
+    }
+
+    private void initializeDropdown() {
+        Spinner spinner = (Spinner) findViewById(R.id.spinner);
+        Athlete[] athletesList = new Athlete[athletes.size()];
+        ArrayAdapter<Athlete> athleteAdapter = new ArrayAdapter<Athlete>(this, R.layout.support_simple_spinner_dropdown_item, athletes.toArray(athletesList));
+        spinner.setAdapter(athleteAdapter);
+
+        RelativeLayout layout = (RelativeLayout) findViewById(R.id.content_main);
+
+        for (int i = 0; i < athletes.get(0).intermediates.length; i++) {
+            Button button = new Button(this);
+            RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+            button.setId(i+1337);
+            button.setText("INT " + (i+1));
+            if (i != 0) {
+                params.addRule(RelativeLayout.RIGHT_OF, button.getId()-1);
+            } else {
+                params.addRule(RelativeLayout.RIGHT_OF, R.id.spinner);
+            }
+
+            button.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View v) {
+                    Spinner spinner = (Spinner) findViewById(R.id.spinner);
+                    Athlete selected = (Athlete) spinner.getSelectedItem();
+                    long time = Calendar.getInstance().getTimeInMillis();
+                    selected.intermediates[v.getId() - 1337] = time;
+                    if (reference == null) {
+                        reference = selected;
+                    }
+                    TableView table = (TableView) findViewById(R.id.main_table);
+                    table.getDataAdapter().notifyDataSetInvalidated();
+                }
+            });
+
+            layout.addView(button, params);
+        }
+    }
+
+    private class AthleteClickListener implements TableDataClickListener<Athlete> {
+        @Override
+        public void onDataClicked(int rowIndex, Athlete athlete) {
+            reference = athlete;
+            TableView table = (TableView) findViewById(R.id.main_table);
+            table.getDataAdapter().notifyDataSetChanged();
+            Spinner spinner = (Spinner) findViewById(R.id.spinner);
+            ArrayAdapter adapter = (ArrayAdapter) spinner.getAdapter();
+            spinner.setSelection(adapter.getPosition(reference));
+        }
+    }
+
+    private void createAthletes() {
+        String[] names = {"Arne", "Per", "Kari", "Kjersti", "Kjell", "Peder", "Rolf"};
+
+        int number = 1;
+        long start = 0;
+
+        athletes = new ArrayList<>();
+
+        for (String name : names
+                ) {
+            athletes.add(new Athlete(name, number, start, 2));
+
+            number++;
+            start += 5*60*1000;
+        }
     }
 }

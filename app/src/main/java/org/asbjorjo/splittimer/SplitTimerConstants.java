2c2be3a1d6fa538ffdbba53158d5227b6d6dfa01
Asbjoern L. Johansen <asbjorjo@gmail.com>
2016-12-07 22:09:53 +0100

    Add intent resultcode for data added.

--- app/src/main/java/org/asbjorjo/splittimer/SplitTimerConstants.java
+++ app/src/main/java/org/asbjorjo/splittimer/SplitTimerConstants.java
@@ -1,5 +1,7 @@
 package org.asbjorjo.splittimer;
 
+import android.app.Activity;
+
 /**
  * Created by AJohansen2 on 11/24/2016.
  */
@@ -11,4 +13,6 @@ public class SplitTimerConstants {
     public static final int ADD_EVENT = 122;
     public static final int BUILD_STARTLIST = 123;
     public static final int BUILD_INTERMEDIATES = 124;
+
+    public static final int RESULT_ADDED = Activity.RESULT_FIRST_USER + 42;
 }
